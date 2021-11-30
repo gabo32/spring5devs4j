573ad4acfb2dec5c05aa76fbd015570d60e0f114
Juan Gabriel Briceño <juan.briseno@gnp.com.mx>
2021-11-29 23:02:16 -0600

    [gabo32] actualizacion de queryparams

--- users-app/src/main/java/com/gabo32/dev4j/users/UsersAppApplication.java
+++ users-app/src/main/java/com/gabo32/dev4j/users/UsersAppApplication.java
@@ -1,13 +1,18 @@
 package com.gabo32.dev4j.users;
 
+import java.util.HashMap;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class UsersAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersAppApplication.class, args);
+		//HashMap<K, V>
 	}
 
 }
