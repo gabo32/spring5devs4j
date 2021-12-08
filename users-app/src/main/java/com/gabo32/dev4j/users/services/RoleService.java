7de441f7d0ce7cb448e31590ba0aa5710df5d995
JuanGab <j.gabriel.b@live.com.mx>
2021-12-08 01:48:42 -0600

    [gabo32] spring security 2

--- users-app/src/main/java/com/gabo32/dev4j/users/services/RoleService.java
+++ users-app/src/main/java/com/gabo32/dev4j/users/services/RoleService.java
@@ -3,17 +3,24 @@ package com.gabo32.dev4j.users.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.gabo32.dev4j.users.entities.Role;
 import com.gabo32.dev4j.users.entities.User;
+import com.gabo32.dev4j.users.modelos.Devs4jSecurityRule;
 import com.gabo32.dev4j.users.repositories.RoleRepository;
 import com.gabo32.dev4j.users.repositories.UserInRoleRepository;
 
 @Service
+@Devs4jSecurityRule
 public class RoleService {
 
 	@Autowired	
@@ -22,7 +29,12 @@ public class RoleService {
 	@Autowired
 	private UserInRoleRepository inRoleRepository;
 	
-	public List<User> getUsersByRole(String rolName){
+	//@Secured({"ROLE_ADMIN"})
+	//@RolesAllowed({"ROLE_ADMIN"})
+	//@PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+	//@PostAuthorize("hasRole('ROLE_ADMIN')")
+	//@Devs4jSecurityRule
+	public List<User> getUsersByRole(String rolName){ 
 		return inRoleRepository.findUserByRoleName(rolName);
 	}
 	
